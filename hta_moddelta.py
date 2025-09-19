c0dfb76bd18ef9a722a7fd50aa9d8260e4fd4289
Dmitrii Sashchenko <gnome627@hrbox.io>
2025-09-19 15:01:31 +0600

    version 2.0

--- hta_moddelta.py
+++ hta_moddelta.py
@@ -1,78 +1,68 @@
 import os
 import filecmp
 import shutil
+from easygui import diropenbox
 from datetime import datetime
 import yaml
 
+version='2.0'
+
 try:
-    with open('moddelta_config.yaml', 'r', encoding='utf-8') as f:
+    with open('mdelta.yaml', 'r', encoding='utf-8') as f:
         config = yaml.safe_load(f)
+        mdeltaignore = config['ignore']
 except:
+    config = None
+    mdeltaignore = [
+        'data\\profiles',
+        'data\\editor\\newmap',
+        'data\\editor\\DiffMasks',
+        'data\\editor\\settings.xml',
+        'data\\config.cfg',
+        'data\\m3deditor.cfg',
+        'data\\gsmed.cfg',
+        '.bak\\',
+        '.ssl.bak' 
+    ]
     print('🚫 файл конфигурации не найден')
-    input('Нажмите клавишу Any...')
-    quit()
-
-def get_dir_entries(get_dirs: bool = True, sub: str = None):
-    obj_list = os.listdir(config['base_path'])
-    if get_dirs:
-        filtered_list = [obj for obj in obj_list if os.path.isdir(f'{config["base_path"]}\\{obj}')]
-    else:
-        filtered_list = [obj for obj in obj_list if os.path.isfile(f'{config["base_path"]}\\{obj}')]
-    if sub == None:
-        return(filtered_list)
-    else:
-        return([dir for dir in filtered_list if sub in dir and not sub == dir])
-
-def select_mod_folder(mod_folders: list[str]):
-    while True:
-        choosen_mod_folder_number = input('\n🍞 Введите номер директории, содержащей Вашу модификацию: ')
-        try:
-            numer = int(choosen_mod_folder_number) - 1
-            if mod_folders[numer]:
-                print(f'  • Выбрана директория: {mod_folders[numer]}')
-                return mod_folders[numer]
-        except ValueError:
-            print('  🚫 неправильный формат ввода!!')
 
 
-def copy_delta(modded_game_name: str):
+def copy_delta(original_game: str, modded_game: str):
     '''
     modded_game_name - ИМЯ папки, в которой лежит игра с установленной модификацией.
     '''
-    destination_dir = f'ModDelta {modded_game_name} {datetime.now().strftime("%y-%m-%d-%H%M%S")}'
+    modded_game_name = modded_game.split(os.path.sep)[-1]
+    destination_dir = f'{datetime.now().strftime("%y-%m-%d-%H%M%S")} {modded_game_name}'
     os.makedirs(destination_dir)
-    modded_game_data = f'{config["base_path"]}\\{modded_game_name}\\data'
+    modded_game_data = f'{modded_game}\\data'
     for root, dir, objects in os.walk(modded_game_data):
         for obj in objects:
-            obj_fullpath = f'{root}\\{obj}'
-            orig_obj_fullpath = obj_fullpath.replace(modded_game_name, config['hta_folder_name'])
-            if not any(v in obj_fullpath for v in config['ignore']) and (not os.path.exists(orig_obj_fullpath) or os.path.exists(orig_obj_fullpath) and not filecmp.cmp(obj_fullpath, orig_obj_fullpath)):
-                destination_fullpath = f'{destination_dir}\\{obj_fullpath.split(modded_game_name)[1]}'
-                os.makedirs(os.path.dirname(destination_fullpath), exist_ok=True)
-                shutil.copy2(obj_fullpath, destination_fullpath)
-                print(f'    💾 {obj_fullpath.split(modded_game_name)[1]} скопирован!')
+            obj_path = f'{root}\\{obj}'
+            orig_obj_path = f'{root.replace(modded_game, original_game)}\\{obj}'
+            # print(f'Сравнивается {obj_path} и {orig_obj_path}')
+            if not any(v in obj_path for v in mdeltaignore) and (not os.path.exists(orig_obj_path) or os.path.exists(orig_obj_path) and not filecmp.cmp(obj_path, orig_obj_path)):
+                destination_fullpath = f'{destination_dir}\\{obj_path.split(modded_game_name)[1]}'
+                os.makedirs(
+                    os.path.dirname(destination_fullpath),
+                    exist_ok=True
+                )
+                shutil.copy2(obj_path, destination_fullpath)
+                print(f'    💾 {obj_path.split(modded_game_name)[1]} скопирован!')
     print(f'\n🎉 Файлы модификации успешно сохранены в {destination_dir}')
         
     
 
-print(f'Вас приветствует ♻️   HTA ModDelta ver. {config["version"]} \n')
-possible_mod_folders = get_dir_entries(sub = config['hta_folder_name'])
+print(f'Вас приветствует ♻️   HTA ModDelta ver. {version} \n')
+original_game = diropenbox(
+    msg = 'Выберите директорию с ИСХОДНОЙ (оригинальной/до модификации) игрой.',
+    title = 'Выбор исходной игры'
+)
+print(f'  🍞 Выбрана исходная директория: {original_game}')
+modded_game = diropenbox(
+    msg = 'Выберите директорию с МОДИФИЦИРОВАННОЙ игрой.',
+    title = 'Выбор модифицированной игры'
+)
+print(f'  🥐 Сравнение с модификацией: {modded_game}')
 
-if len(possible_mod_folders) > 1:
-    for index, mod in enumerate(possible_mod_folders):
-        print(f'{index + 1} • {mod}')
-    mod_folder_name = select_mod_folder(possible_mod_folders)
-elif len(possible_mod_folders) == 1:
-    mod_folder_name = possible_mod_folders[0]
-else:
-    raw_dir = input('🍞 Введите имя директории, содержащей Вашу модификацию: ')
-    if os.path.isdir(f'{config["base_path"]}\\{raw_dir}'):
-        print(f'  • Выбрана директория: {raw_dir}')
-        mod_folder_name = raw_dir
-    else:
-        print('🚫 модификации не найдены')
-        input('Нажмите клавишу Any...')
-        quit()
-
-copy_delta(mod_folder_name)
+copy_delta(original_game, modded_game)
 input('Нажмите что угодно для продолжения...')
